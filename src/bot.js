3635bf99df647a88a7ef68fbbab82ea358bfde27
EC2 Default User <ec2-user@ip-172-31-15-228.eu-north-1.compute.internal>
2022-10-09 20:35:56 +0000

    Bug fixes

--- src/bot.js
+++ src/bot.js
@@ -138,7 +138,7 @@ bot.command('today', async (ctx) => {
     const schedule = await getSchedule(group.groupId);
 
     let week = 'scheduleFirstWeek';
-    if (now.currentWeek === 2) week = 'scheduleSecondWeek';
+    //week = 'scheduleSecondWeek';
 
     const todaysPairs = sortPairs(schedule[week][now.currentDay - 1]);
     let message = `*${WEEKDAYS[now.currentDay - 1]}*` + '\n\n';
@@ -174,7 +174,7 @@ bot.command('tomorrow', async (ctx) => {
     const schedule = await getSchedule(group.groupId);
 
     let week = 'scheduleFirstWeek';
-    if (now.currentWeek === 2) week = 'scheduleSecondWeek';
+    //week = 'scheduleSecondWeek';
 
     const todmorrowsPairs = sortPairs(schedule[week][now.currentDay]);
     let message = `*${WEEKDAYS[now.currentDay]}*` + '\n\n';
@@ -245,7 +245,7 @@ bot.command('week', async ctx => {
   
     const now = await currentTime();
     let week = 'scheduleFirstWeek';
-    if (now.currentWeek === 2) week = 'scheduleSecondWeek';
+    //week = 'scheduleSecondWeek';
   
   
     const schedule = (await getSchedule(group.groupId))[week];
@@ -283,7 +283,7 @@ bot.command('nextweek', async ctx => {
   
     const now = await currentTime();
     let week = 'scheduleSecondWeek';
-    if (now.currentWeek === 2) week = 'scheduleFirstWeek';
+    //week = 'scheduleFirstWeek';
   
   
     const schedule = (await getSchedule(group.groupId))[week];
